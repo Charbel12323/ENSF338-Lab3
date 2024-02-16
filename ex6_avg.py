4cda89acd3d53314a202bf19194a23a68ff181c6
MohamadG04 <mohamadghattas04@gmail.com>
2024-02-15 20:19:19 -0700

    Finished 6

--- ex6_avg.py
+++ ex6_avg.py
@@ -1,5 +1,5 @@
-import time
 import random
+import time
 import matplotlib.pyplot as plt
 
 def linear_search(arr, target):
@@ -26,29 +26,35 @@ def measure_performance(algorithm, input_size):
     target = random.choice(arr)
 
     start_time = time.time()
-    result = algorithm(arr, target)
+    algorithm(arr, target)
     end_time = time.time()
 
     return end_time - start_time
 
+# Test the algorithms on 100 random tasks
 input_sizes = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000]
-
 linear_search_times = []
 binary_search_times = []
 
 for size in input_sizes:
-    linear_time = measure_performance(linear_search, size)
-    binary_time = measure_performance(binary_search, size)
+    linear_search_total_time = 0
+    binary_search_total_time = 0
 
-    linear_search_times.append(linear_time)
-    binary_search_times.append(binary_time)
+    for _ in range(100):
+        linear_search_total_time += measure_performance(linear_search, size)
+        binary_search_total_time += measure_performance(binary_search, size)
 
+    linear_search_avg_time = linear_search_total_time / 100
+    binary_search_avg_time = binary_search_total_time / 100
 
+    linear_search_times.append(linear_search_avg_time)
+    binary_search_times.append(binary_search_avg_time)
+
+# Plot the results
 plt.plot(input_sizes, linear_search_times, label='Linear Search')
 plt.plot(input_sizes, binary_search_times, label='Binary Search')
 plt.xlabel('Input Size')
-plt.ylabel('Execution Time (s)')
-plt.title('Algorithm Performance Comparison')
+plt.ylabel('Average Time (s)')
 plt.legend()
 plt.show()
 
