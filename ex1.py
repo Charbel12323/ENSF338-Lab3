3808e27cfe49909ad820175e5ceac4de978e5921
MohamadG04 <mohamadghattas04@gmail.com>
2024-02-15 21:09:48 -0700

    Removed Comment.

--- ex1.py
+++ ex1.py
@@ -49,6 +49,3 @@ a=[43, 72, 43, 73, 54, 5, 48, 26, 59, 73]
 
 merge_sort(a,0,(len(a) -1))
 print(a)
-
-
-#2) The array is recursively split into two halves until subarrays of size 1 are left.Since each time we perform the split, we divide the array size by 2. Hence, the depth of the recursion tree is log n.The merging process requires comparing elements from both subarrays, which can take up to n comparisons in the worst case.Since there are log n levels (depth of the recursion) and at each level we perform O(n) work (to merge all elements at that level), the total work done is O(n log n).
